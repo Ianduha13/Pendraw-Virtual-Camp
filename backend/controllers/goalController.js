87344abd08a5d08cf081b1772b36bf6b1cd4a5b1
Ian Duhamel <ianduhamelhayes@gmail.com>
2022-11-16 15:38:33 -0300

    Adding error handling and middleware

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,15 +1,31 @@
-const getGoals = (req, res)=>{
+const asyncHandler = require('express-async-handler')
+
+const getGoals = asyncHandler(async (req, res)=>{
   res.status(200).json({message: 'get goals'})
-}
-const setGoals = (req, res)=>{
+})
+
+const setGoals = asyncHandler(async (req, res)=>{
+  if(!req.body.text){
+    res.status(400)
+    throw new Error('Please add a text field')
+  }
+
+
+
   res.status(200).json({message: 'set goals'})
-}
-const updateGoal = (req, res)=>{
+
+})
+
+const updateGoal = asyncHandler(async (req, res)=>{
   res.status(200).json({message: `Update goal ${req.params.id}`})
-}
-const deleteGoal = (req, res)=>{
+})
+
+const deleteGoal = asyncHandler(async (req, res)=>{
   res.status(200).json({message: `Delete goal ${req.params.id}`})
-}
+})
+
+
+
 module.exports={
   getGoals,
   setGoals,
