193396007cc8c31df5dc861aeef38ca5992f07ca
Ian Duhamel <ianduhamelhayes@gmail.com>
2022-11-25 10:07:06 -0300

    Production  url

--- frontend/src/features/auth/authService.js
+++ frontend/src/features/auth/authService.js
@@ -1,25 +1,25 @@
-import axios from 'axios'
+import axios from "axios"
 
-const API_URL = '/api/users/'
+const API_URL = "https://pendraw-campus-backend.vercel.app/api/users"
 
 const register = async (userData) => {
   const response = await axios.post(API_URL, userData)
-  if(response.data){
-    localStorage.setItem('user', JSON.stringify(response.data))
+  if (response.data) {
+    localStorage.setItem("user", JSON.stringify(response.data))
   }
   return response.data
 }
 const login = async (userData) => {
   const response = await axios.post(`${API_URL}/login`, userData)
-  if(response.data){
-    localStorage.setItem('user', JSON.stringify(response.data))
+  if (response.data) {
+    localStorage.setItem("user", JSON.stringify(response.data))
   }
   return response.data
 }
 
 const authService = {
   register,
-  login
+  login,
 }
 
 export default authService
