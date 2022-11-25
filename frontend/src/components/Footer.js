fd0f8a9f0fd20b0a934ffd92b61c771888ca0153
Ian Duhamel <ianduhamelhayes@gmail.com>
2022-11-25 15:08:01 -0300

    Adding styles

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -5,33 +5,34 @@ import "./styles/Footer.css"
 const Footer = () => {
   return (
     <footer>
-      <p>Designed by Ian Duhamel 2022</p>
-      <button className='btn-options btn-login'>
-        <a
-          href='https://www.linkedin.com/in/ian-duhamel/'
-          target={"_blank"}
-          rel='noreferrer'
-        >
-          <FaLinkedin />
-          Linkedin
-        </a>
-      </button>
-      <button className='btn-options btn-login'>
-        <a
-          href='https://github.com/Ianduha13'
-          target={"_blank"}
-          rel='noreferrer'
-        >
-          <FaGithubSquare />
-          Github
-        </a>
-      </button>
-      <button className='btn-options btn-login'>
-        <a href='/'>
-          <FaUserTie />
-          Portfolio
-        </a>
-      </button>
+      <div className='buttonContainer'>
+        <button className='btn-footer btn-login'>
+          <a
+            href='https://www.linkedin.com/in/ian-duhamel/'
+            target={"_blank"}
+            rel='noreferrer'
+          >
+            <FaLinkedin />
+            Linkedin
+          </a>
+        </button>
+        <button className='btn-footer btn-login'>
+          <a
+            href='https://github.com/Ianduha13'
+            target={"_blank"}
+            rel='noreferrer'
+          >
+            <FaGithubSquare />
+            Github
+          </a>
+        </button>
+        <button className='btn-footer btn-login'>
+          <a href='/'>
+            <FaUserTie />
+            Portfolio
+          </a>
+        </button>
+      </div>
     </footer>
   )
 }
--- frontend/src/pages/Profile.js
+++ frontend/src/pages/Profile.js
@@ -2,13 +2,28 @@ import React from "react"
 import "../components/styles/Profile.css"
 
 const Profile = ({ user }) => {
-  const userName = JSON.stringify(user.name)
-  const email = JSON.stringify(user.email)
+  const userNamejson = JSON.stringify(user.name)
+  const emailjson = JSON.stringify(user.email)
+  const userName = userNamejson.replace(/"([^"]+)":/g, "$1:")
+  const email = emailjson.replace(/"([^"]+)":/g, "$1:")
   return (
     <div className='profile-container'>
+      <div className='photo'>
+        <button>dd your photo</button>
+      </div>
       <div className='data-container'>
         <p>Name: {userName}</p>
         <p>Email: {email}</p>
+      </div>
+      <div className='bot-container'>
+        <div className='history-container'>
+          <h2>Post History</h2>
+          <section>Make a post to add it to your history</section>
+        </div>
+        <div className='classroom-container'>
+          <h2>Your Classrooms</h2>
+          <section>Join a Classroom to add it to your history</section>
+        </div>
       </div>
     </div>
   )
