db1cc34d180c76d1b5e8691c31dfca0657adc995
Ian Duhamel <ianduhamelhayes@gmail.com>
2022-11-30 13:12:43 -0300

    Changing functions on vercel.json

--- api/controllers/userController.js
+++ api/controllers/userController.js
@@ -1,22 +1,20 @@
-const jwt = require ('jsonwebtoken')
-const bcrypt = require('bcryptjs')
-const asyncHandler = require('express-async-handler')
-const User = require('../model/userModel')
+const jwt = require("jsonwebtoken")
+const bcrypt = require("bcryptjs")
+const asyncHandler = require("express-async-handler")
+const User = require("../model/userModel")
 
-
-
-const registerUser = asyncHandler(async(req, res) =>{
+const registerUser = asyncHandler(async (req, res) => {
   const { name, email, password } = req.body
 
-  if ( !name || !email || !password ){
+  if (!name || !email || !password) {
     res.status(400)
-    throw new Error ('Please add all fields')
+    throw new Error("Please add all fields")
   }
-  const userExist = await User.findOne({email})
+  const userExist = await User.findOne({ email })
 
-  if (userExist){
+  if (userExist) {
     res.status(400)
-    throw new Error('User already exist')
+    throw new Error("User already exist")
   }
 
   const salt = await bcrypt.genSalt(10)
@@ -25,57 +23,55 @@ const registerUser = asyncHandler(async(req, res) =>{
   const user = await User.create({
     name,
     email,
-    password: hashedPassword
+    password: hashedPassword,
   })
-  if(user){
+  if (user) {
     res.status(201).json({
       _id: user.id,
       name: user.name,
       email: user.email,
-      token: generateToken(user._id)
+      token: generateToken(user._id),
     })
-  }
-  else {
+  } else {
     res.status(400)
-    throw new Error('Invalid user data')
+    throw new Error("Invalid user data")
   }
-  res.json ({message: 'Register User'})
+  res.json({ message: "Register User" })
 })
 
-const loginUser = asyncHandler( async (req, res) =>{
-  const {email, password} = req.body
+const loginUser = asyncHandler(async (req, res) => {
+  const { email, password } = req.body
 
-  const user = await User.findOne({email})
-  if(user && (await bcrypt.compare(password, user.password))){
+  const user = await User.findOne({ email })
+  if (user && (await bcrypt.compare(password, user.password))) {
     res.json({
       _id: user.id,
       name: user.name,
       email: user.email,
-      token: generateToken(user._id)
+      token: generateToken(user._id),
     })
   } else {
     res.status(400)
-    throw new Error('Invalid credentials')
+    throw new Error("Invalid credentials")
   }
 })
 
-const getUser = asyncHandler( async (req, res) =>{
-  const {_id, name, email} = await User.findById(req.user.id)
-  
+const getUser = asyncHandler(async (req, res) => {
+  const { _id, name, email } = await User.findById(req.user.id)
+
   res.status(200).json({
-    id:_id,
+    id: _id,
     name,
     email,
   })
 })
 
-
-const generateToken = (id) =>{
-  return jwt.sign({ id }, process.env.JWT_SECRET, {expiresIn: '30d'})
+const generateToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: "30d" })
 }
 
-module.exports ={
+module.exports = {
   registerUser,
   loginUser,
-  getUser
+  getUser,
 }
