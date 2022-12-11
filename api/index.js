15fcb71569addc245d1a1f52091caf0cec755989
Ian Duhamel <ianduhamelhayes@gmail.com>
2022-12-10 23:11:45 -0300

    Fixing index.js

--- api/index.js
+++ api/index.js
@@ -1,24 +1,21 @@
 const path = require("path")
 const express = require("express")
+const cors = require("cors")
 const dotenv = require("dotenv").config()
 const { errorHandler } = require("../middleware/errorMiddleware")
 const connectDB = require("../config/db")
-const cors = require("cors")
-const bodyParse = require("body-parser")
 
 connectDB()
 
 const app = express()
 
-app.use(express.json())
 app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+app.use(errorHandler)
 
 app.use("/api/posts", require("../routes/postRoutes"))
 app.use("/api/users", require("../routes/userRoutes"))
-
-app.use(bodyParse.urlencoded({ extended: true }))
-app.use(bodyParse.json())
-
-app.use(errorHandler)
 
 export default app
