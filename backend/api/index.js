f81ddfe97d03ea35d4e044de9c512bc22c8770e9
Ian Duhamel <ianduhamelhayes@gmail.com>
2022-12-10 22:48:06 -0300

    Adding vercel.json features

--- backend/api/index.js
+++ backend/api/index.js
@@ -2,8 +2,8 @@ const path = require("path")
 const express = require("express")
 const colors = require("colors")
 const dotenv = require("dotenv").config()
-const { errorHandler } = require("./middleware/errorMiddleware")
-const connectDB = require("./config/db")
+const { errorHandler } = require("../middleware/errorMiddleware")
+const connectDB = require("../config/db")
 const port = process.env.PORT || 5000
 const cors = require("cors")
 const logger = require("morgan")
@@ -15,8 +15,8 @@ const app = express()
 
 app.use(express.json())
 
-app.use("/api/posts", require("./routes/postRoutes"))
-app.use("/api/users", require("./routes/userRoutes"))
+app.use("/api/posts", require("../routes/postRoutes"))
+app.use("/api/users", require("../routes/userRoutes"))
 
 app.use(bodyParse.urlencoded({ extended: true }))
 app.use(bodyParse.json())
---
+++ backend/config/db.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose")
+
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI)
+  } catch (error) {
+    console.log(error)
+    process.exit(1)
+  }
+}
+module.exports = connectDB
