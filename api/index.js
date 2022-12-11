a50c0b1a65f4fb80609a836e021b6a4a4d12dc75
Ian Duhamel <ianduhamelhayes@gmail.com>
2022-12-10 22:55:58 -0300

    Fixing index.js

--- api/index.js
+++ api/index.js
@@ -14,6 +14,7 @@ connectDB()
 const app = express()
 
 app.use(express.json())
+app.use(cors())
 
 app.use("/api/posts", require("../routes/postRoutes"))
 app.use("/api/users", require("../routes/userRoutes"))
@@ -21,18 +22,6 @@ app.use("/api/users", require("../routes/userRoutes"))
 app.use(bodyParse.urlencoded({ extended: true }))
 app.use(bodyParse.json())
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/build")))
-  app.get("*", (req, res) =>
-    res.sendFile(
-      path.resolve(__dirname, "../", "frontend", "build", "index.html")
-    )
-  )
-} else {
-  app.get("/", (req, res) => res.send("Please set to production"))
-}
 app.use(errorHandler)
-
-app.listen(port, () => console.log(`Server started on port ${port}`))
 
 export default app
