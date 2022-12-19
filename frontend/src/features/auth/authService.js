86ede9e754adf5d548ca1d81e6a0b97f9ecf4acc
Ian Duhamel <ianduhamelhayes@gmail.com>
2022-12-19 11:00:47 -0300

    Fixing auth service reference url

--- frontend/src/features/auth/authService.js
+++ frontend/src/features/auth/authService.js
@@ -2,14 +2,14 @@ import axios from "axios"
 const apiUrl = process.env.REACT_APP_API_URL
 
 const register = async (userData) => {
-	const response = await axios.post(`${apiUrl}`, userData)
+	const response = await axios.post(`${apiUrl}/users/`, userData)
 	if (response.data) {
 		localStorage.setItem("user", JSON.stringify(response.data))
 	}
 	return response.data
 }
 const login = async (userData) => {
-	const response = await axios.post(`${apiUrl}/login`, userData)
+	const response = await axios.post(`${apiUrl}/users/login`, userData)
 	if (response.data) {
 		localStorage.setItem("user", JSON.stringify(response.data))
 	}
